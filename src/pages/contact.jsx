5b31172fd9942f587ae608b17c84e3efc61a42af
Linda Dick <linda@aureuscattus.org>
2023-08-27 11:54:10 -0500

    Another attempt to use Recaptcha V2

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,102 +1,82 @@
-import { Helmet } from 'react-helmet'
 import Layout from '../components/layout'
 import React from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
 
 const ContactFormPage = () => {
-  // const recaptchaSiteKey = process.env.SITE_RECAPTCHA_KEY
-
-  // useEffect(() => {
-  //   window.onSubmit = onSubmit
-  // })
-
-  // const onSubmit = (e) => {
-  //   console.log('submitting')
-  //   console.log(e)
-  //   return true
-  // }
-
   return (
-    <>
-      <Helmet>
-        {/* <script
-          key="recaptcha"
-          type="text/javascript"
-          src="https://www.google.com/recaptcha/api.js"
-        /> */}
-      </Helmet>
-      <Layout pageTitle="Contact the Author">
-        <h1>Contact the Author</h1>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          data-netlify-recaptcha="true">
-          <input type="hidden" name="form-name" value="contact" />
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="form-control"
-              required={true}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="form-control"
-              required={true}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="subject" className="form-label">
-              Subject
-            </label>
-            <input
-              type="text"
-              name="subject"
-              id="subject"
-              className="form-control"
-              required={true}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="message" className="form-label">
-              Message
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              rows="5"
-              className="form-control"
-              required={true}
-            />
-          </div>
-          <div data-netlify-recaptcha="true"></div>
-          <div className="mb-3">
-            <button
-              type="submit"
-              // data-sitekey={recaptchaSiteKey}
-              // data-callback="onSubmit"
-              // data-action="submit"
-              className="g-recaptcha btn btn-primary me-2">
-              Send
-            </button>
-            <button type="reset" className="btn btn-secondary">
-              Clear
-            </button>
-          </div>
-        </form>
-      </Layout>
-    </>
+    <Layout pageTitle="Contact the Author">
+      <h1>Contact the Author</h1>
+      <form
+        name="contact"
+        method="POST"
+        action="/thank-you"
+        data-netlify="true"
+        data-netlify-recaptcha="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <input
+          type="hidden"
+          name="subject"
+          value="robinet.co.uk Contact Form Message"
+        />
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="form-control"
+            required={true}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="form-control"
+            required={true}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message-subject" className="form-label">
+            Subject
+          </label>
+          <input
+            type="text"
+            name="message-subject"
+            id="message-subject"
+            className="form-control"
+            required={true}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            rows="5"
+            className="form-control"
+            required={true}
+          />
+        </div>
+        <ReCAPTCHA sitekey="{process.env.GATSBY_RECAPTCHA_KEY}" />
+        <div className="mb-3">
+          <button type="submit" className="btn btn-primary me-2">
+            Send
+          </button>
+          <button type="reset" className="btn btn-secondary">
+            Clear
+          </button>
+        </div>
+      </form>
+    </Layout>
   )
 }
 
