de1a4836e803c3912b4afeec9c0dd230bd0f16a0
Linda Dick <linda@aureuscattus.org>
2023-12-03 13:20:19 -0600

    Move contact form to a client-side page

--- app/components/recaptcha-div.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import ReCAPTCHA from 'react-google-recaptcha'
-
-export default function ReCAPTCHADiv() {
-  return (
-    <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string} />
-  )
-}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReCAPTCHADiv from '../components/recaptcha-div'
+import ContactForm from '../components/contact-form'
 
 export const metadata = {
   title: 'Contact the Author',
@@ -9,78 +9,7 @@ export default function ContactFormPage() {
   return (
     <>
       <h1>Contact the Author</h1>
-      <form
-        name="contact"
-        method="POST"
-        action="/thank-you"
-        data-netlify="true"
-        data-netlify-recaptcha="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <input
-          type="hidden"
-          name="subject"
-          value="robinet.co.uk Contact Form Message"
-        />
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="form-control"
-            required={true}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="form-control"
-            required={true}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="message-subject" className="form-label">
-            Subject
-          </label>
-          <input
-            type="text"
-            name="message-subject"
-            id="message-subject"
-            className="form-control"
-            required={true}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="message" className="form-label">
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            rows={5}
-            className="form-control"
-            required={true}
-          />
-        </div>
-        <div className="mb-3">
-          <ReCAPTCHADiv />
-        </div>
-        <div className="mb-3">
-          <button type="submit" className="btn btn-primary me-2">
-            Send
-          </button>
-          <button type="reset" className="btn btn-secondary">
-            Clear
-          </button>
-        </div>
-      </form>
+      <ContactForm />
     </>
   )
 }
